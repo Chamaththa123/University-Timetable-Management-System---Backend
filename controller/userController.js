f32036def8b5a8c529280b7967a2f386fe8c11ab
Chamaththa123 <shamodchamaththa@gmail.com>
2024-06-15 16:57:39 +0530

    add user validation for register and login

--- controller/userController.js
+++ controller/userController.js
@@ -6,6 +6,18 @@ async function registerUser(req, res) {
   try {
     const userData = req.body;
 
+    if (!userData.firstName) {
+      return res.status(400).json({ message: "firstName is required" });
+    } else if (!userData.lastName) {
+      return res.status(400).json({ message: "lastName is required" });
+    } else if (!userData.email) {
+      return res.status(400).json({ message: "email is required" });
+    } else if (!userData.role) {
+      return res.status(400).json({ message: "role is required" });
+    } else if (!userData.password) {
+      return res.status(400).json({ message: "password is required" });
+    }
+
     const existingUser = await User.findOne({ email: userData.email });
 
     if (existingUser) {
@@ -25,6 +37,13 @@ async function registerUser(req, res) {
 async function loginUser(req, res) {
   try {
     const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ message: "email is required" });
+    } else if (!password) {
+      return res.status(400).json({ message: "password is required" });
+    }
+
     const token = await userService.loginUser(email, password);
     res.status(200).json({ token });
   } catch (error) {
